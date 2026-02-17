36b2a93c39e1e24e43021d3e5f131172500c3139
VergaJU <jacopoumberto.verga@gmail.com>
2026-02-17 14:50:04 +0000

    Standardize packaging: consolidate dependencies in pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
-
-setup(
-    name="BorderGraph",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=required,
-    python_requires=">=3.8",
-)
+setup()
